92f98694068ccf7f6540f636376bab16f515e261
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-09-18 13:49:01 -0700

    add README file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # E-commerce Back End Starter Code
+
+![License](https://img.shields.io/badge/License%3A-MIT-green.svg)
+
+## Description
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Tech Used](#tech-used)
+- [Questions](#questions)
+- [Links](#links)
+- [License](#license)
+
+## Installation
+
+- Both Node.js and MySQL must be installed on your computer.
+- Navigate to the root directory and run:
+  - `npm install`
+- To start the server, in the command line run:
+  - `npm start`
+
+## Tech Used
+
+- Node
+- Express
+- MySQL
+- Sequelize
+- Dotenv
+
+## Questions
+
+Please don't hesitate to reach out if you have any suggestions or feedback
+
+## Links
+
+- **[GitHub Repo](https://github.com/mjos7/my-ecomm-backend-store)**
+- **[Video Walkthrough]()**
+
+## License
+
+![License](https://img.shields.io/badge/License%3A-MIT-green.svg)
+
+This app is licensed under the MIT license.
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// GET all categories
 router.get('/', (req, res) => {
-  // find all categories
   // be sure to include its associated Products
   Category.findAll({
     include: {
@@ -19,9 +19,8 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET one category by id
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id,
@@ -38,8 +37,8 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// POST a new category
 router.post('/', (req, res) => {
-  // create a new category
   Category.create({
     category_name: req.body.category_name,
   })
@@ -50,8 +49,8 @@ router.post('/', (req, res) => {
     });
 });
 
+// PUT update a category
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update(
     {
       category_name: req.body.category_name,
@@ -75,8 +74,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// DELETE a category by id
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id,
